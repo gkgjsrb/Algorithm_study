67bf1687dbbc9591c3cda61091441dec91bdf7a3
gkgjsrb <gkgjsrb@gmail.com>
2018-08-30 18:41:30 +0900

    https://www.acmicpc.net/problem/9012

--- 괄호.cpp
+++ 괄호.cpp
@@ -10,27 +10,27 @@ int main()
 	string input;
 	cin >> tc;
 	for (int i = 0; i < tc; i++) {
-		stack<char> st;
-		bool fail = false;
+		stack<char> st; // 매 반복마다 스택이 초기화되어야 하기 때문에 내부에 선언
+		bool fail = false; // 위와 같은 이유
 		cin >> input;
 		for (auto it : input) {
 			if (it == '(') {
-				st.push(it);
+				st.push(it); // 괄호가 여는 괄호이면 스택에 푸쉬
 			}
-			else if (it == ')') {
-				if (st.empty() == true) {
-					fail = true;
+			else if (it == ')') { // 괄호가 닫는 괄호인데
+				if (st.empty() == true) { // 스택이 비어 있다면 안된다
+					fail = true; // 실패로 표시
 					break;
 				}
-				else {
+				else { // 스택이 비어있지 않다면 여는 괄호를 꺼낸다
 					st.pop();
 				}
 			}
 		}
-		if (st.empty() == true && fail==false) {
+		if (st.empty() == true && fail==false) { // 모든 조합이 완성되어 스택이 비어 있다면 yes 출력
 			cout << "YES" << '\n';
 		}
-		else {
+		else { // 아니면 no 
 			cout << "NO" << '\n';
 		}
 	}
